9356050c7f9a2a06597b25154ded40f295c7faf8
Edson Caliman <edsonjunior@db1.com.br>
2020-04-19 17:03:48 -0300

    Atividade 2 aula 3
    Utilizar useEffect
    Alterar valor padrão do repeat para utilizar useEffect
    Apresentar um log a cada alteração da variável repeat
    Criar dois contadores para quando clicar no incrementar ou
    decrementar e exibir no final da página

--- src/Logo.js
+++ src/Logo.js
@@ -1,31 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./Logo.css";
 
 const Logo = (props) => {
-  const [repeat, setRepeat] = useState(1);
+  const [repeat, setRepeat] = useState();
+  const [countPlus, setCountPlus] = useState(0);
+  const [countMinus, setCountMinus] = useState(0);
   const imgs = [];
+
+  // executa apenas uma vez na criação do componente
+  useEffect(() => {
+    setRepeat(1);
+  }, []);
+
+  // executa na alteração do estado da variavel repeat
+  useEffect(() => {
+    console.log("repeat updated!");
+  }, [repeat]);
 
   for (let i = 0; i < repeat; i++) {
     imgs.push(<img key={i} src={logo} className="App-logo" alt="logo" />);
   }
 
+  const handleClickPlus = () => {
+    setRepeat((repeat) => repeat + 1);
+    setCountPlus((count) => count + 1);
+  };
+  const handleClickMinus = () => {
+    setRepeat((repeat) => repeat - 1);
+    setCountMinus((count) => count + 1);
+  };
+
   return (
     <>
       <div className="App-logo-container">{imgs}</div>
       <div className="App-logo-buttons">
-        <button
-          disabled={repeat === 6}
-          onClick={() => setRepeat((repeat) => repeat + 1)}
-        >
+        <button disabled={repeat === 6} onClick={handleClickPlus}>
           Incrementar
         </button>
-        <button
-          disabled={repeat === 1}
-          onClick={() => setRepeat((repeat) => repeat - 1)}
-        >
+        <button disabled={repeat === 1} onClick={handleClickMinus}>
           Decrementar
         </button>
+      </div>
+      <div>
+        <p>
+          <b>Logo +</b> button was clicked {countPlus} times
+        </p>
+        <p>
+          <b>Logo -</b> button was clicked {countMinus} times
+        </p>
       </div>
     </>
   );
